01a85ab837689cead158214dd52fff345d2ff727
Raph Roberts <github@raphroberts.com>
2020-05-19 10:55:39 +1000

    Added support for multiple word grids

---
+++ js/data-dynamic-bodies.js
@@ -0,0 +1,19 @@
+// INSECT BODY PARTS
+
+// Wordlist data
+// 0,1 = X/Y start letter, 2,3 = X/Y end letter, 4 = word, 5=colour
+
+var wordList = [
+    [102, 410, 280, 410, 'foreleg', '0,255,255,0.5'],
+    [39, 200, 131, 111, 'head', '0,255,0,0.5'],
+    [309, 202, 309, 351, 'thorax', '0,0,255,0.5']
+];
+
+// Grid of letters
+
+var letterString = "\
+
+";
+
+// Columns in this grid
+var columnLength = 10;
